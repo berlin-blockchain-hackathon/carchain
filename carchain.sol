cc35eb3d4f9c4232c8ba9e80f2685dcf970f9095
joerasa <joerasa@gmx.de>
2018-03-16 10:41:29 +0100

    Dealer: Payment introduced, owner instead of customer

--- carchain.sol
+++ carchain.sol
@@ -3,11 +3,35 @@ contract CarChain  {
 /////////////////////// Description ///////////////////////////////
 // car has own account
 // Manufactures registers car.
-// Buyer can retrieve kfz Brief and Zulassung
+// Car can be sold and paiid by nwe owner
+// Owner  can retrieve kfz Brief and Zulassung
+// Owner  can retrieve inspections
+// Owner  can retrieve milage insrted by Car
+// car canm pay and sell services
 
 
 /////////////////////// Declarations ///////////////////////////////
   
+    // // Public variables of the token
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+ 
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
+
+
+
+   
+   
     // Data of a single firm like carproducer, part manufacturer, repair workshop
     
     address provider; //provider of the whole contract
@@ -21,6 +45,8 @@ contract CarChain  {
         bytes32 street;
         Typeenum firmType;
         bool registered;
+        address[] myCarAccts;
+        RepairDone[] repairsDone;    
         
     }
      
@@ -37,9 +63,18 @@ contract CarChain  {
     event carInfo(
        address eCarAddress,
        bytes32 eType,
-       bytes32 eSerial
+       bytes32 aCarSerial
+       
     );
     
+ 
+    event carSold(
+        address aCarAdr, 
+        address _to, 
+        uint _price,
+        uint counter
+        );
+ 
  
     // Data of a single milage entry
      struct Milage {
@@ -56,6 +91,23 @@ contract CarChain  {
         address insertedBy;
     }
     
+    // Data of a single part entry
+     struct Repair {
+        bytes32 reason;
+        bytes32 details; 
+        uint    timestamp;
+        address doneBy;
+        uint    price;
+        bool    paid;
+    }
+    
+    struct RepairDone{
+        address theCarAdr;
+        uint counter; 
+    }
+
+    
+
 
     // Data of a single car
      struct Car {
@@ -63,9 +115,12 @@ contract CarChain  {
         bytes32  cartype;
         address  producer;
         address  owner;
+        address  nextOwner;
+        uint     price;
         bool     registered;
         Milage[] milages;
         Part[]   parts;
+        Repair[] repairs;
      }
 
      // dynamic structure of cars
@@ -75,18 +130,78 @@ contract CarChain  {
      
    
 /////////////////////// Functions ///////////////////////////////
-
-    function CarChain() public {
+   /**
+     * Constructor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    function CarChain(
+        uint256 initialSupply
+    ) public {
+        totalSupply = initialSupply; 
+        balanceOf[msg.sender] = totalSupply;                    // Give the creator all initial tokens
+        name = "carCoin";                                       // Set the name for display purposes
+        symbol = "CC€";                                         // Set the symbol for display purposes
         provider = msg.sender;
-        // todo assign car contigents
-        }
+
+    }
+
+    /**
+     *  transfer
+     */
+    function transfer(address _to, uint _value) public  {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[msg.sender] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[msg.sender] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[msg.sender] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[msg.sender] + balanceOf[_to] == previousBalances);
+    }
+
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function createCoins(uint256 _value) public returns (bool success) {
+        require(msg.sender == provider);       // Check if the sender  is the bank
+        totalSupply += _value;                 // Updates totalSupply
+        return true;
+    }
+
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burnCoins(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
+    }
+ 
 
  
     function registerFirm (bytes32 aFirmName, bytes32 aFirmLocation,bytes32 aFirmStreet, Typeenum aFirmType) public returns (uint retno) {
         firms[msg.sender].nameOfFirm=aFirmName;
         firms[msg.sender].city=aFirmLocation;
         firms[msg.sender].street=aFirmStreet;
-         firms[msg.sender].firmType=aFirmType;
+        firms[msg.sender].firmType=aFirmType;
    
         if (firms[msg.sender].registered != true) {firmAccts.push(msg.sender) -1;}
         firms[msg.sender].registered=true;
@@ -99,7 +214,7 @@ contract CarChain  {
     }   
 
     function setMilage (address aCarAdr, uint32 miles)  public returns (uint _noEntries) {
-        if (msg.sender != cars[aCarAdr].owner) return; 
+        require ((msg.sender == cars[aCarAdr].owner)||(msg.sender == aCarAdr)); 
 
         Milage  memory currentMilage;
         currentMilage.timestamp=now;
@@ -110,55 +225,16 @@ contract CarChain  {
     }
 
     function getMilage (address aCarAdr, uint32 _noEntries)  view public returns (uint32, uint) {
-        if (msg.sender != cars[aCarAdr].owner) return; 
+        require ((msg.sender == cars[aCarAdr].owner)||(msg.sender == aCarAdr)); 
         uint32  _milage= cars[aCarAdr].milages[_noEntries].milage;
         uint  _timestamp= cars[aCarAdr].milages[_noEntries].timestamp;
         
         return (_milage, _timestamp);
     }
     
-    // register car is called by car manufacturer
-    function registerCar (address aCarAdr, bytes32 aCarType, bytes32 aCarSerial) public returns (uint _noOfCars) {
-        //  precondition: manufacture has created a car account 
-        //  private key is stored in car computer  and at manufacturer
-        
-   
-       if (firms[msg.sender].firmType != Typeenum.carmanufacturer) return; 
-        //  carmanufacturers can create cars only 
-        cars[aCarAdr].carSerial=aCarSerial;
-        cars[aCarAdr].cartype=aCarType;
-        cars[aCarAdr].producer=msg.sender;
-        cars[aCarAdr].owner=msg.sender;
-        setMilage(aCarAdr,0);
-        
-        if (cars[aCarAdr].registered != true) {carAccts.push(aCarAdr) -1;}
-        cars[aCarAdr].registered=true;
-
-        carInfo(aCarAdr,aCarType, aCarSerial);
-
-        return carAccts.length;
-        
-    }
-    
-    function displayCar (address aCarAdr)  public constant returns (bytes32, bytes32, bytes32, bytes32, bytes32, bytes32){
-       return (
-           firms[cars[aCarAdr].producer].nameOfFirm, 
-           firms[cars[aCarAdr].owner].nameOfFirm,
-           firms[cars[aCarAdr].owner].city,
-           firms[cars[aCarAdr].owner].street,
-           cars[aCarAdr].cartype, 
-           cars[aCarAdr].carSerial
-           );
-    }   
-
-    function sellCar (address aCarAdr, address _to) public {
-        if (msg.sender != cars[aCarAdr].owner) return; 
-        cars[aCarAdr].owner= _to;
-    }
-
 
     function setPart (address aCarAdr, bytes32 _partNo, bytes32 _partType)  public  returns (uint _noEntries) {
-        if (msg.sender != cars[aCarAdr].owner )  return; 
+        require (msg.sender == cars[aCarAdr].owner); 
 
         Part  memory currentPart;
         currentPart.timestamp   = now;
@@ -170,7 +246,7 @@ contract CarChain  {
     }
 
     function getPart (address aCarAdr, uint32 _noEntries)  view public returns (bytes32, bytes32, uint, bytes32) {
-        if (msg.sender != cars[aCarAdr].owner ) return; 
+        require (msg.sender == cars[aCarAdr].owner); 
         bytes32  _partNo= cars[aCarAdr].parts[_noEntries].partNo;
         bytes32  _partType= cars[aCarAdr].parts[_noEntries].partType;
         uint  _timestamp= cars[aCarAdr].parts[_noEntries].timestamp;
@@ -180,8 +256,198 @@ contract CarChain  {
         return (_partNo, _partType, _timestamp,inserterName);
     }
 
+    function setRepair (address aCarAdr, bytes32 _reason, bytes32 _details, uint _price)  public  returns (uint _noEntries) {
+        require (
+            (msg.sender == cars[aCarAdr].owner) || 
+            (firms[msg.sender].firmType == Typeenum.workshop)
+            ); 
+
+        Repair  memory currentRepair;
+        currentRepair.timestamp   = now;
+        currentRepair.reason      = _reason;
+        currentRepair.details     = _details;
+        currentRepair.doneBy      = msg.sender;
+        currentRepair.price       = _price;
+        if (_price > 0) 
+            currentRepair.paid    = false; 
+            else 
+            currentRepair.paid    = true ;
+        
+        cars[aCarAdr].repairs.push(currentRepair) -1;
+        
+        RepairDone memory currentRepairDone;
+        currentRepairDone.theCarAdr = aCarAdr;
+        currentRepairDone.counter = cars[aCarAdr].repairs.length;
+        firms[msg.sender].repairsDone.push(currentRepairDone) -1;
+        
+        return cars[aCarAdr].repairs.length;
+    }
+
+    function getRepair (address aCarAdr, uint32 _noEntries)  view public returns (uint, bytes32,bytes32,bytes32, uint, bool){
+        require (msg.sender == cars[aCarAdr].owner); 
+        
+        Repair  memory currentRepair;
+        currentRepair.timestamp   = cars[aCarAdr].repairs[_noEntries].timestamp;
+        currentRepair.reason      = cars[aCarAdr].repairs[_noEntries].reason;
+        currentRepair.details     = cars[aCarAdr].repairs[_noEntries].details;
+        currentRepair.doneBy      = cars[aCarAdr].repairs[_noEntries].doneBy;
+        currentRepair.price       = cars[aCarAdr].repairs[_noEntries].price;
+        currentRepair.paid        = cars[aCarAdr].repairs[_noEntries].paid;
+  
+        bytes32    repairFirmName= firms[currentRepair.doneBy].nameOfFirm;
+  
+        return (
+             currentRepair.timestamp  ,
+             currentRepair.reason     ,
+             currentRepair.details    ,
+             repairFirmName           ,
+             currentRepair.price      ,
+             currentRepair.paid       
+        );
+    }
+ 
+     function displayRepairDone (uint32 _repNoFirm)  public constant returns (uint, bytes32,bytes32, bytes32, uint, bool){
+       /*     require (
+                (firms[msg.sender].firmType == Typeenum.partproducer)||
+                (firms[msg.sender].firmType == Typeenum.seller)||
+                (firms[msg.sender].firmType == Typeenum.workshop)
+            );
+       because of VM errer:revert*/ 
+        
+            address repAdr;
+            uint repNoCar;
+            repAdr    = firms[msg.sender].repairsDone[_repNoFirm].theCarAdr;
+            repNoCar   = firms[msg.sender].repairsDone[_repNoFirm].counter; 
+            
+        Repair  memory curRep;
+        curRep.timestamp   = cars[repAdr].repairs[repNoCar].timestamp;
+        curRep.reason      = cars[repAdr].repairs[repNoCar].reason;
+        curRep.details     = cars[repAdr].repairs[repNoCar].details;
+        curRep.doneBy      = cars[repAdr].repairs[repNoCar].doneBy;
+        curRep.price       = cars[repAdr].repairs[repNoCar].price;
+        curRep.paid        = cars[repAdr].repairs[repNoCar].paid;
+  
+        bytes32 repairFirmName    = firms[curRep.doneBy].nameOfFirm;
+
+  
+        return (
+             curRep.timestamp  ,
+             curRep.reason     ,
+             curRep.details    ,
+             repairFirmName    ,
+             curRep.price      ,
+             curRep.paid       
+        );
+    }
+ 
+   
+    function payRepair (address aCarAdr,uint index) public {
+        require (balanceOf[msg.sender] >= cars[aCarAdr].repairs[index].price);
+        require (cars[aCarAdr].repairs[index].paid == false);
+   
+        transfer(cars[aCarAdr].repairs[index].doneBy,cars[aCarAdr].repairs[index].price);
+        cars[aCarAdr].repairs[index].paid = true;
+    }
+
+
+    // register car is called by car manufacturer
+    function registerCar (address aCarAdr, bytes32 aCarType, bytes32 aCarSerial) public returns (uint _noOfCars) {
+        //  precondition: manufacture has created a car account 
+        //  private key is stored in car computer  and at manufacturer
+        
+   
+        require (firms[msg.sender].firmType == Typeenum.carmanufacturer); 
+        require (aCarAdr != 0x0); 
+ 
+        cars[aCarAdr].carSerial=aCarSerial;
+        cars[aCarAdr].cartype=aCarType;
+        cars[aCarAdr].producer=msg.sender;
+        cars[aCarAdr].owner=msg.sender;
+        cars[aCarAdr].nextOwner=0x0;
+        
+        setMilage(aCarAdr,0);
+        setRepair(aCarAdr, "Factory test","final QA check", 0);
+        
+        if (cars[aCarAdr].registered != true) {carAccts.push(aCarAdr) -1;}
+        if (cars[aCarAdr].registered != true) {firms[msg.sender].myCarAccts.push(aCarAdr) -1;}
+         
+        cars[aCarAdr].registered=true;
+
+        carInfo(aCarAdr,aCarType, aCarSerial);
+
+        return carAccts.length;
+        
+    }
+    
+    function displayCar (address aCarAdr)  public constant returns (bytes32, bytes32, bytes32, bytes32, bytes32, bytes32, uint){
+       return (
+           firms[cars[aCarAdr].producer].nameOfFirm, 
+           firms[cars[aCarAdr].owner].nameOfFirm,
+           firms[cars[aCarAdr].owner].city,
+           firms[cars[aCarAdr].owner].street,
+           cars[aCarAdr].cartype, 
+           cars[aCarAdr].carSerial,
+           cars[aCarAdr].price
+           );
+    }   
+
+    function displayMyCar (uint _counter)  public constant returns (bytes32, bytes32, bytes32, bytes32, bytes32, bytes32, uint, address){
+           address myCarAdr;
+           myCarAdr= firms[msg.sender].myCarAccts[_counter];
+        return (
+           firms[cars[myCarAdr].producer].nameOfFirm, 
+           firms[cars[myCarAdr].owner].nameOfFirm,
+           firms[cars[myCarAdr].owner].city,
+           firms[cars[myCarAdr].owner].street,
+           cars[myCarAdr].cartype, 
+           cars[myCarAdr].carSerial,
+           cars[myCarAdr].price,
+           myCarAdr
+           );
+    }   
+
+ 
+
+    function sellCar (address aCarAdr, address _to, uint _price) public {
+        
+        require (msg.sender == cars[aCarAdr].owner);
+        require (cars[aCarAdr].nextOwner == 0x0); 
+        require (cars[aCarAdr].registered == true);  //it has to be a car
+
+        uint counter;
+        cars[aCarAdr].nextOwner = _to; 
+
+        if(_price > 0) {
+                cars[aCarAdr].price = _price; 
+            } else {
+                cars[aCarAdr].owner =_to;
+            }
+        
+        firms[_to].myCarAccts.push(aCarAdr) -1;
+        counter=firms[_to].myCarAccts.length;
+        carSold (aCarAdr, _to, _price, counter);
+
+    }
     
     
+    function payCar (address aCarAdr) public {
+        require (msg.sender == cars[aCarAdr].nextOwner);
+        require (balanceOf[msg.sender] >= cars[aCarAdr].price);
+   
+        transfer(cars[aCarAdr].owner,cars[aCarAdr].price);
+        cars[aCarAdr].nextOwner = 0x0; 
+        cars[aCarAdr].owner= msg.sender;
+    }
+    
+    function claimCar (address aCarAdr) public {
+        require (msg.sender == cars[aCarAdr].owner);
+        require (cars[aCarAdr].nextOwner != 0x0); 
+
+        cars[aCarAdr].nextOwner =0x0; 
+    }
+ 
+    
+
     function getNoOfCars () public view returns (uint retno) {
        return carAccts.length;
     }
@@ -198,7 +464,10 @@ contract CarChain  {
        return carAccts[counter];
     }
       
-    
-    
+    function getMyCarAdr (uint counter) public view returns (address retno) {
+       return firms[msg.sender].myCarAccts[counter];
+    }
+      
+   
 // end of functions    
 }
